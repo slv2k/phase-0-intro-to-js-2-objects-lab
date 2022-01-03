710a6a56ed5723b275acdf52ee025f143b8b1eb9
samuel <samuelmmanor@gmail.com>
2022-01-03 13:15:03 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const { objDisplay } = require("chai/lib/chai/utils");
+
 // Write your solution in this file!
+const employee = {
+    name: "John",
+    streetAddress: "123 Smith Street"
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return Object.assign({},obj,{[key]:value});
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key]=value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newEmployee = {...obj, ...key};
+    delete newEmployee[key]
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key]
+    return obj;
+}
